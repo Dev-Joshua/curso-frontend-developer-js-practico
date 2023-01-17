c022173c81dfd7855103032e68ba909c032bf5da
Dev-Joshua <Joshua_dup@hotmail.com>
2023-01-17 12:18:04 -0500

    Termino fusion del menu integrando varios componentes(menu-desktop y mobile-menu)

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 //Selecciono al elemento el cual le dare clic
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuBurgerIcon = document.querySelector(".menu-burger");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 //Utilizo su metodo addEventListener para ejecutar algo cuando se de clic al elemento
 menuEmail.addEventListener("click", showDesktopMenu);
+menuBurgerIcon.addEventListener("click", toggleMobileMenu);
 
 //Esta funcion hace aparecer o desaparecer la clase .inactive. Se ejecuta cada vez que den clic
 function showDesktopMenu() {
   console.log("Click");
   //Ejecuta la funcion classList.toggle que quita o pone la clase 'inactive' dependiendo si la tiene o no
   desktopMenu.classList.toggle("inactive");
+}
+
+//Aparece o desaparece el menu mobile
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
